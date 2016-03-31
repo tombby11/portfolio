79c38b5ad0de6855b6c265ab1d696bc5a70abc26
tombby11 <tombby11@gmail.com>
2016-03-31 21:45:45 +0200

    Adding VLC

---
+++ VideoStreaming/VideoStreaming/Views/VlcStreamer.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Controls;
+using  Vlc.DotNet.Wpf;
+using VlcLibDirectoryNeededEventArgs = Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs;
+
+namespace VideoStreaming.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for VlcStreamer.xaml
+    /// </summary>
+    public partial class VlcStreamer : UserControl, IStreamer
+    {
+        public VlcStreamer()
+        {
+            
+            InitializeComponent();
+            Player.MediaPlayer.VlcLibDirectoryNeeded += OnVlcControlNeedsLibDirectory;
+            Player.MediaPlayer.EndInit();
+        }
+
+        private void OnVlcControlNeedsLibDirectory(object sender, VlcLibDirectoryNeededEventArgs e)
+        {
+            var currentAssembly = Assembly.GetEntryAssembly();
+            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+            var parentDirectory = System.IO.Directory.GetParent(currentDirectory).FullName;
+            //packages\Vlc.DotNet.Wpf.2.1.122\lib\net45\x86
+            if (currentDirectory == null)
+                return;
+            if (AssemblyName.GetAssemblyName(currentAssembly.Location).ProcessorArchitecture == ProcessorArchitecture.X86)
+                e.VlcLibDirectory = new DirectoryInfo(Path.Combine(parentDirectory, @"Lib\x86"));
+            else
+                e.VlcLibDirectory = new DirectoryInfo(Path.Combine(parentDirectory, @"Lib\x64"));
+        }
+
+        public void Play(Uri channel)
+        {
+            Player.MediaPlayer.Stop();
+            Player.MediaPlayer.Play(channel);
+        }
+    }
+}
---
+++ VideoStreaming/VideoStreaming/Views/WindowsPlayerStreamer.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VideoStreaming.WPF.Views
+{
+    /// <summary>
+    ///     Interaction logic for WindowsPlayerStreamer.xaml
+    /// </summary>
+    public partial class WindowsPlayerStreamer : IStreamer
+    {
+        public WindowsPlayerStreamer()
+        {
+            InitializeComponent();
+        }
+
+        public void Play(Uri channel)
+        {
+            Player.Stop();
+            Player.Source = channel;
+            Player.Play();
+        }
+    }
+}
